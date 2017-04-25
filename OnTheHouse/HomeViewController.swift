ad2e20cd474e2caa031e29665234f9da2b5a0f5c
aditya7395 <aditya7395@gmail.com>
2017-04-25 15:16:26 -0700

    Basic UI done

---
+++ OnTheHouse/HomeViewController.swift
@@ -0,0 +1,64 @@
+//
+//  HomeViewController.swift
+//  GroupPay
+//
+//  Created by Michael Hyun on 4/21/17.
+//  Copyright © 2017 CMPE137. All rights reserved.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    var password : String?
+    var username : String?
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+
+//        let identifier = segue.identifier
+//        switch identifier! {
+//            case "Account":
+//                if let accountVC = segue.destination as? AccountViewController{
+//                    //data send
+//                }
+//            case "Group":
+//                if let groupVC = segue.destination as? GroupsViewController{
+//                    //data send
+//                }
+//            case "Events":
+//                if let eventsVC = segue.destination as? EventsViewController{
+//                    //data send
+//                }
+//            case "History":
+//                if let historyVC = segue.destination as? HistoryViewController{
+//                    //data send
+//                }
+//            case "Settings":
+//                if let settingsVC = segue.destination as? SettingsViewController{
+//                    //data send
+//                }
+//            case "Reminders":
+//                if let remindersVC = segue.destination as? RemindersViewController{
+//                    //data send
+//                }
+//            default:
+//                return
+//        }
+    }
+ 
+
+}

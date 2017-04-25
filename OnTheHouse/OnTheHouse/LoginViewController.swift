55d20a7c75083dc852b423ee4bbb5b16ae605ff8
michaelhyun <michael.hyun3@gmail.com>
2017-04-25 16:11:27 -0700

    Added Signup Functionality

--- OnTheHouse/OnTheHouse/LoginViewController.swift
+++ OnTheHouse/OnTheHouse/LoginViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  LoginViewController.swift
 //  OnTheHouse
 //
 //  Created by Michael Hyun on 4/25/17.
@@ -8,18 +8,22 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class LoginViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        // Do any additional setup after loading the view.
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
 
+
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+    }
 
 }
-

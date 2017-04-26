dec9b640c1e38db21fc6c87294fef525ff52bfd0
michaelhyun <michael.hyun3@gmail.com>
2017-04-26 00:59:10 -0700

    Facebook Login Button made

--- OnTheHouse/OnTheHouse/LoginViewController.swift
+++ OnTheHouse/OnTheHouse/LoginViewController.swift
@@ -7,23 +7,17 @@
 //
 
 import UIKit
+import FBSDKLoginKit
 
 class LoginViewController: UIViewController {
 
+    @IBOutlet var loginButton: FBSDKLoginButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+                
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-    
 
-
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-    }
 
 }

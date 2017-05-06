ab4d06231e409f5f61d7d54911906e64900be5f9
michaelhyun <michael.hyun3@gmail.com>
2017-05-05 18:57:47 -0700

    Deleted segue for signup, used code instead

--- OnTheHouse/OnTheHouse/SignUpViewController.swift
+++ OnTheHouse/OnTheHouse/SignUpViewController.swift
@@ -33,7 +33,8 @@ class SignUpViewController: UIViewController {
                 
                 if error == nil {
                     print("You have successfully signed up")
-                    self.performSegue(withIdentifier: "register", sender: self)
+                    let vc = self.storyboard?.instantiateViewController(withIdentifier: "Home")
+                    self.present(vc!, animated: true, completion: nil)
                     
                 } else {
                     self.displayAlert(userMessage: "Please complete the form correctly")
@@ -43,30 +44,7 @@ class SignUpViewController: UIViewController {
 
     }
 
-    
-    override func shouldPerformSegue(withIdentifier identifier: String, sender: Any?) -> Bool {
-        let userEmail = _email.text!
-        let userPass = _password.text!
-        let userPassConfirm = _confirmpassword.text!
-        let userFirst = _firstname.text!
-        
-        if(identifier == "CreateUserSegue"){
-            if ((userEmail.isEmpty)||(userPass.isEmpty)||(userFirst.isEmpty)||(userPassConfirm.isEmpty)){
-                //display error message
-                displayAlert(userMessage: "All fields are required")
-                return false;
-            }
-            if(userPassConfirm != userPass){
-                //display alert message
-                displayAlert(userMessage: "Passwords do not match")
-                return false
-            }
-            
-            
-        }
-        return true
-        
-    }
+
     
     func displayAlert(userMessage: String){
         let alert = UIAlertController(title: "Alert", message: userMessage, preferredStyle: UIAlertControllerStyle.alert)
@@ -78,9 +56,7 @@ class SignUpViewController: UIViewController {
         self.present(alert, animated: true, completion:nil)
     }
     
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        
-    }
+
     @IBAction func backToLoginPressed(_ sender: Any) {
         self.dismiss(animated: true, completion: nil)
     }
